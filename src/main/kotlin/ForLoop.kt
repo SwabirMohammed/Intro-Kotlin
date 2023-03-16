17c208315690ea616248b81894080c0cf34ae2ca
Swabir <swabir49@gmail.com>
2023-03-16 09:56:38 +0300

    Loops in Kotlin

--- src/main/kotlin/ForLoop.kt
+++ src/main/kotlin/ForLoop.kt
@@ -1,7 +1,10 @@
 fun main(args: Array<String>) {
 
-   for(i in 5..50){
-       println(i)
+   for(i in 1..100){
+       if (i % 5 == 0){
+           println(i)
+       } else println("Not Divisible")
+
    }
 
 
