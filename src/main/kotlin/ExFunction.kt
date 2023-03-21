3ce60afa2eeaeb55e5a09a0a44381c5067e44c4c
Swabir <swabir49@gmail.com>
2023-03-21 09:56:32 +0300

    Functions in Kotlin

---
+++ src/main/kotlin/ExFunction.kt
@@ -0,0 +1,28 @@
+fun main(args: Array<String>) {
+    addnumbers()
+    multiplynumbers()
+    dividenumbers()
+}
+
+
+fun addnumbers(){
+    val val1 = 500
+    val val2 = 300
+    val sum = val1 + val2
+    println(sum)
+}
+
+fun multiplynumbers(){
+    val val1 = 500
+    val val2 = 4
+    val multiplication = val1 * val2
+    println(multiplication)
+}
+
+
+fun dividenumbers(){
+    val val1 = 100
+    val val2 = 5
+    val division = val1 / val2
+    println(division)
+}
---
+++ src/main/kotlin/Functions.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+
+    sayhello()
+}
+
+
+fun sayhello(){
+    println("Good Morning User!")
+}
+
+fun today(){
+    println("Today is Tuesday!")
+}
